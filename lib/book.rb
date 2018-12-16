347a89144758f1199ccf14316bcbecb46f415377
Cliff Jurgensen <jivefox.002@gmail.com>
2018-12-16 20:13:51 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,32 +6,8 @@ class Book
   def title
     @title
   end
-  
-  attr_accessor :author
 
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @num = num
-  end
-
-  def page_count (num)
-    @num
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre (genre)
-    @genre
-  end
+  attr_accessor :author, :page_count, :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
